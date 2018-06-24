d2177190c0881b0a8385d6721a2e04c034604bc6
anoff <offenhaeuser@gmail.com>
2018-06-24 17:12:24 +0200

    :star2: feat: position blips on button

--- src/components/Radar.vue
+++ src/components/Radar.vue
@@ -1,14 +1,23 @@
 <template>
-  <div>
+  <v-container>
     <div>
       <new-blib></new-blib>
+      <v-btn
+      v-on:click="arrangeBlips()">
+        Arrange
+      </v-btn>
     </div>
-      <div class="radar">
-        <div class="xline"></div>
-        <div class="yline"></div>
-      <div id="blips"></div>
-
-      <div class="q1"><h3>Cloud Technologies</h3></div>
+    <div class="radar" ref="radar">
+      <div class="xline"></div>
+      <div class="yline"></div>
+      <div id="blips" v-for="blip in blips" v-bind:key="blip.id">
+        <a class="blip" rel="tooltip" v-bind:href="blip.link" target="_blank" v-bind:data-category="blip.category" v-bind:data-status="blip.status" v-bind:data-changed="blip.changed" v-bind:title="blip.title"></a>
+      </div>
+      <div class="q1"><h3>Cloud Technologies</h3>
+        <ul v-for="blip in blips" v-bind:key="blip.id">
+          <li v-if="blip.category === 'cloud'">{{blip.title}}</li>
+        </ul>
+      </div>
       <div class="q2"><h3>Tools</h3></div>
       <div class="q3"><h3>Datascience</h3></div>
       <div class="q4"><h3>Backend</h3></div>
@@ -18,8 +27,7 @@
       <div class="assess"></div>
       <div class="hold"></div>
     </div>
-
-  </div>
+  </v-container>
 </template>
 
 <script>
@@ -33,7 +41,81 @@ export default {
     }
   },
   data: () => ({
-  })
+  }),
+  methods: {
+    arrangeBlips () {
+      function getDomWidth (domClass) {
+        console.log(domClass)
+        return document.getElementsByClassName(domClass)[0].clientWidth
+      }
+      const blips = document.getElementsByClassName('blip')
+      for (const b of blips) {
+        const bWidth = b.clientWidth
+        const category = b.dataset.category
+        const status = b.dataset.status
+        console.log(category, status)
+
+        // Different radiuses depending on blips
+        let width, radius
+        switch (status) {
+          case 'hold':
+            radius = (getDomWidth('radar') - bWidth) / 2
+            width = (getDomWidth('radar') - bWidth) / 2 - (getDomWidth('assess') - bWidth) / 2
+            break
+          case 'assess':
+            radius = (getDomWidth('assess') - bWidth) / 2
+            width = (getDomWidth('assess') - bWidth) / 2 - (getDomWidth('trial') - bWidth) / 2
+            break
+          case 'trial':
+            radius = (getDomWidth('trial') - bWidth) / 2
+            width = (getDomWidth('trial') - bWidth) / 2 - (getDomWidth('adopt') - bWidth) / 2
+            break
+          case 'adopt':
+            radius = (getDomWidth('adopt') - bWidth) / 2
+            width = radius = (getDomWidth('adopt') - bWidth) / 2
+            break
+        }
+
+        // Different quadrants depending on area
+        let quadrant
+        switch (category) {
+          case 'tools':
+            quadrant = 1
+            break
+          case 'cloud':
+            quadrant = 2
+            break
+          case 'backend':
+            quadrant = 3
+            break
+          case 'datascience':
+            quadrant = 4
+            break
+        }
+
+        // Calculate things
+        var radarx = (getDomWidth('radar') / 2) // TODO: maybe use height?
+        var radary = (getDomWidth('radar') / 2)
+
+        var rad = radius - width / 2
+        var angle = (quadrant - 1) * Math.PI / 2 + Math.PI / 4
+
+        rad += (Math.sqrt(Math.random()) - 0.5) * width * 0.9
+        angle += (Math.random() - 0.5) * (Math.PI / 2) * 0.9
+
+        let x = rad * Math.cos(angle) + radarx
+        let y = -rad * Math.sin(angle) + radary // use negative values to go up instead of down on x/y pane
+
+        // offset blip size
+        x -= (bWidth / 2)
+        y -= (bWidth / 2)
+
+        // Now position the blip
+        b.style.left = `${x}px`
+        b.style.top = `${y}px`
+      }
+    }
+  }
 }
 </script>
 
@@ -254,6 +336,34 @@ $q4: #A13F52;
     margin: 0 auto;
     display: block;
     text-align: center;
+  }
+}
+
+.blip {
+  display: block;
+  width: 16px;
+  height: 16px;
+  border: 1px solid white;
+  &:hover {
+    animation: waves .1s ease-in-out 1 forwards;
+  }
+  border-radius: 50%;
+  background: $q1;
+  position: relative;
+  top: 0;
+  left: 0;
+  z-index: 100;
+  q1 {
+    background: $q1;
+  }
+  q2 {
+    background: $q2;
+  }
+  q3 {
+    background: $q3;
+  }
+  q4 {
+    background: $q4;
   }
 }
 
