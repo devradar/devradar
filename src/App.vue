4eb516b4d2fbb28964d535ae89c2e1a9ca70c7c7
anoff <offenhaeuser@gmail.com>
2018-06-27 22:52:55 +0200

    :scissors: refactor: move toolbar icons to menu & fix xs radar

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 1    <v-navigation-drawer v-model="drawer" app clipped scroll-off-screen="true" scroll-toolbar-off-screen="true">
       <v-list>
         <v-list-tile
-        v-for="elm in menuItems"
+        v-for="elm in getMenuItems('navbar')"
         v-bind:key="elm.title"
         router
         v-bind:to="elm.link"
@@ -25,7 +25,7 @@
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
         <v-btn flat
-        v-for="elm in menuItems"
+        v-for="elm in getMenuItems('toolbar')"
         v-bind:key="elm.title"
         router
         v-bind:to="elm.link"
@@ -33,6 +33,18 @@
           <v-icon left>{{elm.icon}}</v-icon>
           {{elm.title}}
         </v-btn>
+        <v-menu bottom left v-if="getMenuItems('toolbar-menu').length">
+          <v-btn slot="activator" icon dark>
+            <v-icon>more_vert</v-icon>
+          </v-btn>
+          <v-list>
+            <v-list-tile v-for="elm in getMenuItems('toolbar-menu')" v-bind:key="elm.title" v-bind:to="elm.link" router>
+              <v-list-tile-title>
+                <v-icon left>{{elm.icon}}</v-icon>
+                {{ elm.title }}</v-list-tile-title>
+            </v-list-tile>
+          </v-list>
+        </v-menu>
       </v-toolbar-items>
     </v-toolbar>
     <v-content>
@@ -48,24 +60,21 @@ export default {
   data: () => ({
     drawer: false
   }),
-  props: {
-    source: String
-  },
-  computed: {
-    menuItems: function () {
+  methods: {
+    getMenuItems (location) {
       const user = this.$store.getters.user || {}
       const items = [
-        { icon: 'list', title: 'Blips', link: '/blips', validator: user => user.uid },
-        { icon: 'track_changes', title: 'Radar', link: '/', validator: user => true },
-        { icon: 'delete', title: 'Deprecated', link: '/deprecated', validator: user => user.uid },
-        { icon: 'exit_to_app', title: 'Logout', link: '/logout', validator: user => user.uid },
-        { icon: 'people', title: 'Users', link: '/users', validator: user => user.uid && user.roles.admin },
-        { icon: 'meeting_room', title: 'Login', link: '/login', validator: user => !user.uid }
+        { icon: 'list', title: 'Blips', link: '/blips', validator: user => user.uid, location: ['navbar', 'toolbar'] },
+        { icon: 'track_changes', title: 'Radar', link: '/', validator: user => true, location: ['navbar', 'toolbar'] },
+        { icon: 'delete', title: 'Deprecated', link: '/deprecated', validator: user => user.uid, location: ['navbar', 'toolbar'] },
+        { icon: 'exit_to_app', title: 'Logout', link: '/logout', validator: user => user.uid, location: ['navbar', 'toolbar-menu'] },
+        { icon: 'people', title: 'Users', link: '/users', validator: user => user.uid && user.roles.admin, location: ['navbar', 'toolbar-menu'] },
+        { icon: 'meeting_room', title: 'Login', link: '/login', validator: user => !user.uid, location: ['navbar', 'toolbar'] }
       ]
-      return items.filter(i => i.validator(user))
+      return items
+        .filter(i => i.validator(user))
+        .filter(i => i.location.indexOf(location) > -1)
     }
-  },
-  mounted: function () {
   }
 }
 </script>
--- src/components/Logout.vue
+++ src/components/Logout.vue
@@ -1,6 +1,5 @@
 <template>
   <v-container>
-    <h1>BYE</h1>
   </v-container>
 </template>
 
