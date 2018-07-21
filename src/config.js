941b005c9a5a87e197f3f88fbc41c97a8025728a
anoff <offenhaeuser@gmail.com>
2018-07-21 02:31:15 +0200

    :scissors: refactor: change blip usage to reflect personal history

--- src/config.js
+++ src/config.js
@@ -8,23 +8,23 @@ const config = { // needs to be ES6 module so it can be imported by webpack
     key: 'AIzaSyC3FxfpYywy5ZYpiWuf9nw8_vlbxibpQH8',
     project: 'techradar-f5834'
   },
-  categories: ['Tools', 'Cloud Technologies', 'Backend', 'Datascience'], // quadrant 1-4
-  states: ['Hold', 'Assess', 'Trial', 'Adopt', 'Basics', 'Deprecated'], // should be 0 - 3 for tech radar, 4 for in use, 5 for no longer in use
+  categories: ['Tools', 'Techniques', 'Platforms', 'Languages & Frameworks'], // quadrant 1-4
+  states: ['Know of', 'Tested', 'Used', 'Advanced'], // should be 0 - 3 for tech radar, 4 for in use, 5 for no longer in use
   metaTitle: 'anoff\'s Techradar', // meta information title tag
-  appTitle: 'Tech I work with 🔧', // title showing in the application titlebar
+  appTitle: 'my tech history', // title showing in the application titlebar
   editPermissions: user => user.roles.admin || user.roles.editor,
   routes: [ // configure name, permissions & viewports
     // do NOT change the view property as this links to the vue component and is used for lookups across the app
-    { view: 'List', icon: 'list', title: 'Blips', path: '/list/:search?', validator: user => user.uid, location: ['toolbar'] },
+    { view: 'List', icon: 'list', title: 'Blips', path: '/list/:search?', validator: user => true, location: ['toolbar'] },
     { view: 'Radar', icon: 'track_changes', title: 'Radar', path: '/', validator: user => true, location: ['toolbar'] },
     { view: 'Logout', icon: 'exit_to_app', title: 'Logout', path: '/logout', validator: user => user.uid, location: ['toolbar-menu'] },
     { view: 'Users', icon: 'people', title: 'Users', path: '/users', validator: user => user.uid && user.roles.admin, location: ['toolbar-menu'] },
-    { view: 'Login', icon: 'meeting_room', title: 'Login', path: '/login', validator: user => !user.uid, location: ['toolbar'] }
+    { view: 'Login', icon: 'meeting_room', title: 'Login', path: '/login', validator: user => !user.uid, location: ['toolbar-menu'] }
   ],
   theme: {
-    primary: colors.cyan.darken1,
-    secondary: colors.teal.darken1,
-    accent: colors.purple.lighten2,
+    primary: colors.indigo.base,
+    secondary: colors.blue.darken1,
+    accent: colors.cyan.accent4,
     error: colors.red.base,
     warning: colors.yellow.base,
     info: colors.blue.base,
