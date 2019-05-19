b69d560c62a72988f413a142409d7e8ee1f2244a
Andreas Offenhaeuser <offenhaeuser@gmail.com>
2019-05-19 22:40:06 +0200

    :ant: fix: lint errors

--- src/backend/firebase/store/user.js
+++ src/backend/firebase/store/user.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase/app'
+import 'firebase/auth'
 import 'firebase/firestore'
 
 const ghProvider = new firebase.auth.GithubAuthProvider()
--- src/store/user.js
+++ src/store/user.js
@@ -1,6 +1,3 @@
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
 import Vue from 'vue'
 import appConfig from '../config'
 
