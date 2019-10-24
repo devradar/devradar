1469d4df88b3dcb911635b413dd3cb75eae76753
Andreas Offenhaeuser <offenhaeuser@gmail.com>
2019-10-24 18:28:53 +0100

    :ant: fix(editor): config variations format
    #72

--- editor/config.ts
+++ editor/config.ts
@@ -29,7 +29,6 @@ const config = { // needs to be ES6 module so it can be imported by webpack
       }
     }
   },
-  darkMode: false,
   blips: {
     titleCutOff: 20 // cut off title after N characters (display only)
   },
