5655e8f1c91bc8622ad4542d193aaacd7b6a9882
Andreas Offenhaeuser <offenhaeuser@gmail.com>
2019-11-15 20:21:56 +0100

    :scissors: refactor(app): refactor tests
    #91

--- app/cypress/integration/cookiebanner.spec.js
+++ app/cypress/integration/cookiebanner.spec.js
@@ -2,11 +2,11 @@
 
 context('Application cookie banner', () => {
   it('is shown by default', () => {
-    cy.visit('/')
+    cy.visit('/login')
     cy.get('[data-cy=cookie-banner]').should('be.visible')
   })
   it('gets removed after clicking button', () => {
-    cy.visit('/')
+    cy.visit('/login')
     cy.get('[data-cy=cookie-banner] button').click()
     cy.get('[data-cy=cookie-banner]').should('not.be.visible')
   })
--- app/cypress/integration/radar.spec.js
+++ app/cypress/integration/radar.spec.js
@@ -1,20 +1,14 @@
 /// <reference types="Cypress" />
-
+const devices = ['macbook-15', 'macbook-13', 'iphone-x']
 context('Radar View', () => {
-  ['macbook-15', 'macbook-13', 'iphone-x'].forEach(device => {
-    describe(`works on screensize ${device}`, () => {
-      beforeEach(() => {
-        cy.visit('/@anoff')
-        cy.viewport(device)
-      })
-      it('radar svg is visible', () => {
-        cy.get('[data-cy="radarSvg"]').should('be.visible')
-        cy.get('[data-cy="loadingDialog"]').should('not.be.visible')
-      })
-      it('exactly one legend per side is visible', () => {
-        cy.get('[data-cy="radar-legendwest"]:visible').should('have.length', 1)
-        cy.get('[data-cy="radar-legendeast"]:visible').should('have.length', 1)
-      })
+  it('shows radar + legend responsively', () => {
+    cy.visit('/@anoff')
+    devices.forEach(device => {
+      cy.viewport(device)
+      cy.get('[data-cy="radarSvg"]').should('be.visible')
+      cy.get('[data-cy="loadingDialog"]').should('not.be.visible')
+      cy.get('[data-cy="radar-legendwest"]:visible').should('have.length', 1)
+      cy.get('[data-cy="radar-legendeast"]:visible').should('have.length', 1)
     })
   })
 })
