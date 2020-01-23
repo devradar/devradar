f04c7bb95427caa5c9a05903803d73cae6c75fe9
Andreas Offenhaeuser <offenhaeuser@gmail.com>
2020-01-23 22:34:40 +0100

    :star2: feat(app): use firebase-ui package for oauth login
    #110

--- app/src/components/Login.vue
+++ app/src/components/Login.vue
@@ -3,24 +3,8 @@
     <v-row align="center" justify="center">
       <v-col cols="12" sm="6" lg="4">
         <v-card>
-          <v-card-text>
-            <v-row justify="center">
-              <v-col cols="12" class="login-col login-twitter">
-                <v-btn v-on:click="onLogin('twitter')"
-                data-cy="user-login" mx-auto>
-                <v-icon left>mdi-twitter</v-icon>Login using Twitter</v-btn>
-              </v-col>
-              <v-col cols="12" class="login-col login-github">
-                <v-btn v-on:click="onLogin('github')"
-                data-cy="admin-login" mx-auto>
-                <v-icon left>mdi-github</v-icon>Login using GitHub</v-btn>
-              </v-col>
-              <v-col cols="12" class="login-col login-google">
-                <v-btn v-on:click="onLogin('google')"
-                mx-auto>
-                <v-icon left>mdi-google</v-icon>Login using Google</v-btn>
-              </v-col>
-            </v-row>
+          <v-card-text justify="center">
+            <section id="firebaseui-auth-container"></section>
           </v-card-text>
         </v-card>
       </v-col>
@@ -30,35 +14,24 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import { User } from '@/types/domain'
-import appConfig from '../config'
-import { backend } from '../store'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import * as firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css'
 
-@Component({
-  computed: {
-    user () {
-      return (this.$store.getters.user || {}).uid
-    }
-  }
-})
-
+@Component({})
 export default class NewChange extends Vue {
-  // computed
-  user: User
-
-  public onLogin (provider) {
-    if (appConfig.isUnderTest || appConfig.backend.project === 'devradar-e2e') { // use mocked login in test mode
-      console.warn('Started stubbed login process')
-      backend.test.login()
-    } else {
-      this.$store.dispatch('user/oauthLogin', { provider })
+  public mounted () {
+    const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebase.auth())
+    const uiConfig = {
+      signInSuccessUrl: '/@mine',
+      signInOptions: [
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+      ]
     }
+    ui.start('#firebaseui-auth-container', uiConfig)
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.login-col {
-  text-align: center;
-}
-</style>
