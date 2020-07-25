73cdd054d110228aac298acd4b2ec1d54c3ddbba
anoff <anoff@users.noreply.github.com>
2020-07-25 20:11:52 +0000

    Deploying to gh-pages from master 2793e5567795c1a1de5ff594ee1e807b699aa396

---
+++ init-cookieconsent.js
@@ -0,0 +1,13 @@
+// https://cookieconsent.insites.com/download/#
+window.addEventListener('load', function () {
+  window.cookieconsent.initialise({
+    'palette': {
+      'popup': {
+        'background': '#333'
+      },
+      'button': {
+        'background': '#fff'
+      }
+    }
+  })
+})
